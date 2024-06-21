fd1edaa8f2c6c135552505d4e7a5a885189e6ac3
wadee <85340637+KyrieWade08@users.noreply.github.com>
2024-06-21 11:55:51 +1000

    Create problem1.py
    Today, I created the base code for the first problem of the assignment.

---
+++ problem1.py
@@ -0,0 +1,8 @@
+Celcius1= int(input("What is the temperature in celsius"))
+Fahrenheit1 = (Celcius1 * 9 / 5 + 32)
+
+Fahrenheit2 = int(input("What is the temperature in fahrenheit?"))
+Celcius2 = ((Fahrenheit2-32)/1.8)
+
+print("The Fahrenheit is", Fahrenheit1 )
+print("The Celcius is", Celcius2)
