5917ac7546ce337055adaa58a7cb5862409c54d4
KyrieWade08 <85340637+KyrieWade08@users.noreply.github.com>
2024-07-04 13:20:06 +1000

    Finished problem 21

--- problem1.py
+++ problem1.py
@@ -1,8 +1,11 @@
-Celcius1= int(input("What is the temperature in celsius"))
-Fahrenheit1 = (Celcius1 * 9 / 5 + 32)
+choice = int(input("Type 1 to convert celcius to fahrenheit. \Type 2 to convert fahrenheit to celcius."))
 
-Fahrenheit2 = int(input("What is the temperature in fahrenheit?"))
-Celcius2 = ((Fahrenheit2-32)/1.8)
+if choice == 1:
+    Celcius1= int(input("What is the temperature in celsius"))
+    Fahrenheit1 = (Celcius1 * 9 / 5 + 32)
+    print("The temperature in fahrenheit is", Fahrenheit1, "degrees." )
 
-print("The Fahrenheit is", Fahrenheit1 )
-print("The Celcius is", Celcius2)
+if choice == 2:
+    Fahrenheit2 = int(input("What is the temperature in fahrenheit?"))
+    Celcius2 = ((Fahrenheit2-32)/1.8)
+    print("The temperature in celcius is", Celcius2, "degrees.")
