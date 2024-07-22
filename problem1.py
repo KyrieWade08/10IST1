5fc4cc7e7eacf0dca19cb10f7d7ec9e406cc7849
KyrieWade08 <85340637+KyrieWade08@users.noreply.github.com>
2024-07-22 20:22:22 +1000

    Added error message

--- problem1.py
+++ problem1.py
@@ -1,11 +1,28 @@
-choice = int(input("Type 1 to convert celcius to fahrenheit. \Type 2 to convert fahrenheit to celcius."))
+def main():
+    try:
+        choice = int(input("Type 1 to convert Celsius to Fahrenheit.\nType 2 to convert Fahrenheit to Celsius.\n"))
 
-if choice == 1:
-    Celcius1= int(input("What is the temperature in celsius"))
-    Fahrenheit1 = (Celcius1 * 9 / 5 + 32)
-    print("The temperature in fahrenheit is", Fahrenheit1, "degrees." )
+        if choice == 1:
+            try:
+                Celsius1 = float(input("What is the temperature in Celsius? "))
+                Fahrenheit1 = (Celsius1 * 9 / 5) + 32
+                print("The temperature in Fahrenheit is", Fahrenheit1, "degrees.")
+            except ValueError:
+                print("Please enter a number")
 
-if choice == 2:
-    Fahrenheit2 = int(input("What is the temperature in fahrenheit?"))
-    Celcius2 = ((Fahrenheit2-32)/1.8)
-    print("The temperature in celcius is", Celcius2, "degrees.")
+        elif choice == 2:
+            try:
+                Fahrenheit2 = float(input("What is the temperature in Fahrenheit? "))
+                Celsius2 = (Fahrenheit2 - 32) * 5 / 9
+                print("The temperature in Celsius is", Celsius2, "degrees.")
+            except ValueError:
+                print("Please enter a number.")
+
+        else:
+            print("Please enter 1 or 2.")
+
+    except ValueError:
+        print("Please enter a number.")
+
+if __name__ == "__main__":
+    main()
