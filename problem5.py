ef7eac998cdd914cbcd3a463b161798bff208552
KyrieWade08 <85340637+KyrieWade08@users.noreply.github.com>
2024-07-22 19:40:48 +1000

    Finished and added extra code for problem 5.

--- problem5.py
+++ problem5.py
@@ -1,15 +1,23 @@
-choice = int(input("Enter 1 for addition. \nEnter 2 for subtraction. \nEnter 3 for multiplication\nEnter 4 for division"))
-Num_1=int(input("Type a number"))
-Num_2=int(input("Type a number"))
-if choice == 1:
-    addition=(Num_1+Num_2)
-    print(addition)
-if choice == 2:
-    subtraction=(Num_1-Num_2)
-    print(subtraction)
-if choice == 3:
-    multiplication=(Num_1*Num_2)
-    print(multiplication)
-if choice == 4:
-    division=(Num_1/Num_2)
-    print(division) 
+choice = int(input("Enter 1 for Addition.\nEnter 2 for Subtraction.\nEnter 3 for Multiplication\nEnter 4 for Division\n"))
+
+if choice < 1 or choice > 4:
+    print("Please enter a number between 1 and 4.")
+else:
+    Num_1 = int(input("Type a number: "))
+    Num_2 = int(input("Type another number: "))
+
+    if choice == 1:
+        result = Num_1 + Num_2
+        print(f"{Num_1} plus {Num_2} equals {result}")
+    elif choice == 2:
+        result = Num_1 - Num_2
+        print(f"{Num_1} minus {Num_2} equals {result}")
+    elif choice == 3:
+        result = Num_1 * Num_2
+        print(f"{Num_1} times {Num_2} equals {result}")
+    elif choice == 4:
+        if Num_2 != 0:
+            result = Num_1 / Num_2
+            print(f"{Num_1} divided by {Num_2} equals {result}")
+        else:
+            print("Undefined.")
