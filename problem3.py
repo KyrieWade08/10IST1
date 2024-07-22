f1d8c0dc083bd88441f8ffe6e76f7acc6a5a0d9f
KyrieWade08 <85340637+KyrieWade08@users.noreply.github.com>
2024-07-22 19:30:00 +1000

    Finished Problem 3
    Edited and fixed issues in this problem.

--- problem3.py
+++ problem3.py
@@ -1,7 +1,16 @@
-a = int(input('What is the value of the variable "A"?'))
-b = int(input('What is the value of the variable "B"?'))
-c = int(input('What is the value of the variable "C"?'))
+a = float(input('What is the value of the variable "A"? '))
+b = float(input('What is the value of the variable "B"? '))
+c = float(input('What is the value of the variable "C"? '))
 
-X = (-b + ((b**2 - 4 * a * c)**1/2))/2*a
-Y = (-b - ((b**2 - 4 * a * c)**1/2))/2*a
-print ("The value of X is", X, "and" , Y)
+discriminant = (b**2 - 4*a*c)
+
+if discriminant >= 0:
+    X = (-b + (discriminant**0.5)) / (2*a)
+    Y = (-b - (discriminant**0.5)) / (2*a)
+else:
+    real_part = -b / (2*a)
+    imaginary_part = (-discriminant)**0.5 / (2*a)
+    X = complex(real_part, imaginary_part)
+    Y = complex(real_part, -imaginary_part)
+
+print("The two value  of X are", X, "and", Y)
